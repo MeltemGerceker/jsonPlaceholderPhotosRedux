ce5e124037e25dd6624409a5a5f4adc518e470f0
Meltem Gerceker <meltemy@gmail.com>
2023-08-30 11:23:51 +0300

    refactoring on photoReducer

--- App.jsx
+++ App.jsx
@@ -5,12 +5,10 @@
  * @format
  */
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
-  View,
 } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
